bd6e0f8b3cf4e845007536d566ba9e1511b57002
Divyansh Mittal <dmittal21@iitk.ac.in>
2023-02-03 22:49:29 +0530

    hello

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -112,7 +112,7 @@ const GetAccountBalance = async (OwnerAddress : string) => {
 }
 
 const CreateTransaction = async (OwnerAddress: string, ToAddress: string, Amount: Number) => {
-  const TransactionObject = await fetch('https://api.shasta.trongrid.io/wallet/createtransaction', {
+  const TransactionObject = await (await fetch('https://api.shasta.trongrid.io/wallet/createtransaction', {
     method: 'POST',
     headers: HEADER,
     body: JSON.stringify({
@@ -123,12 +123,12 @@ const CreateTransaction = async (OwnerAddress: string, ToAddress: string, Amount
       visible: true,
       extra_data: 'string'
     })
-  });
+  })).json();
   return { TransactionObject };
 }
 
 const GetTransactionSign = async (TransactionObject: any, PrivateKey: string) => {
-  const TransactionSign = await fetch('https://api.shasta.trongrid.io/wallet/gettransactionsign', {
+  const TransactionSign = await (await fetch('https://api.shasta.trongrid.io/wallet/gettransactionsign', {
     method: 'POST',
     headers: HEADER,
     body: JSON.stringify({
@@ -138,20 +138,20 @@ const GetTransactionSign = async (TransactionObject: any, PrivateKey: string) =>
       },
       privateKey: PrivateKey
     })
-  });
+  })).json();
 
   return { TransactionSign };
 }
 
 const BroadcastTransaction = async () => {
-  const BroadcastResult = await fetch('https://api.shasta.trongrid.io/wallet/broadcasttransaction', {
+  const BroadcastResult = await (await fetch('https://api.shasta.trongrid.io/wallet/broadcasttransaction', {
     method: 'POST',
     headers: HEADER,
     body: JSON.stringify({
       raw_data: '{"contract":[{"parameter":{"value":{"amount":1000,"owner_address":"41608f8da72479edc7dd921e4c30bb7e7cddbe722e","to_address":"41e9d79cc47518930bc322d9bf7cddd260a0260a8d"},"type_url":"type.googleapis.com/protocol.TransferContract"},"type":"TransferContract"}],"ref_block_bytes":"5e4b","ref_block_hash":"47c9dc89341b300d","expiration":1591089627000,"timestamp":1591089567635}',
       raw_data_hex: '0a025e4b220847c9dc89341b300d40f8fed3a2a72e5a66080112620a2d747970652e676f6f676c65617069732e636f6d2f70726f746f636f6c2e5472616e73666572436f6e747261637412310a1541608f8da72479edc7dd921e4c30bb7e7cddbe722e121541e9d79cc47518930bc322d9bf7cddd260a0260a8d18e8077093afd0a2a72e'
     })
-  });
+  })).json();
 }
 
 // const generateAccount = async () => {
